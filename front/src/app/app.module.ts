65b61986ceedbda7622a6bcf7a6a5d156ba561b8
Mohamed Belhaimeur <belhaim789@gmail.com>
2022-02-09 12:33:07 +0100

    routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { CarousselComponent } from './components/caroussel/caroussel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CarousselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/components/caroussel/caroussel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caroussel',
+  templateUrl: './caroussel.component.html',
+  styleUrls: ['./caroussel.component.css']
+})
+export class CarousselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
