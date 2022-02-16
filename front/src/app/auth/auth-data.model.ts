1991c82f5ad19f56bed1383365b8f7dbc0319a85
gu1lhem <sky@anche.no>
2022-02-16 11:39:40 +0100

    WIP auth

---
+++ front/src/app/auth/auth-data.model.ts
@@ -0,0 +1,7 @@
+export interface AuthData{
+    prenom: string;
+    nom: string;
+    email: string;
+    password: string;
+    date_naissance: string;
+}
--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environment } from '../../environments/environment'
+import { AuthData } from './auth-data.model';
 
 @Injectable({providedIn: "root"})
 export class AuthService{
     constructor(private http: HttpClient){}
-    CreateUser(email: string, password: string){
-        this.http.post(environment.api.concat("user/signup"));
+    CreateUser(email: string, password: string, prenom: string, nom: string, date_naissance: string){
+        const authData: AuthData = {
+            email: email,
+            password: password,
+            prenom: prenom,
+            nom: nom,
+            date_naissance: date_naissance
+        }  
+        this.http.post(environment.api.concat("user/signup"), authData)
+            .subscribe(response =>{
+                console.log(response);
+            });
     }
 }
