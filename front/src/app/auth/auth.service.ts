1ae2c08e7d8b0e90843d120a9beeb81856df22ee
gu1lhem <sky@anche.no>
2022-02-16 11:06:55 +0100

    Use env vars

--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { environment } from '../../environments/environment'
 
 @Injectable({providedIn: "root"})
 export class AuthService{
     constructor(private http: HttpClient){}
     CreateUser(email: string, password: string){
-        this.http.post("http://localhost:3000/api/user/signup");
+        this.http.post(environment. "user/signup");
     }
 }
