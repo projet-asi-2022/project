2132235d2eba10989e496e8d720063d8708ee9de
malinvaudpaul <paulmalinvaud@protonmail.com>
2022-02-23 10:08:38 +0100

    Initialisation des services

--- front/src/app/components/admin-articles/details/details.component.ts
+++ front/src/app/components/admin-articles/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ArticlesService } from 'src/app/services/articles.service';
+import { ArticlesService } from 'src/app/services/articles/articles.service';
 
 @Component({
   selector: 'app-details',
